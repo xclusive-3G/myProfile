1bc65e0a7a3bcd3f6d58662e818d7ebdc5652516
unknown <akinrataa@gmail.com>
2024-11-17 05:08:59 +0100

    made few correction in nav

--- src/components/NavBar.jsx
+++ src/components/NavBar.jsx
@@ -1,9 +1,11 @@
 import React from 'react'
+import { useState } from 'react';
 import { CiMenuFries } from "react-icons/ci";
 import {Link} from "react-scroll"
-
+import { MdClose } from "react-icons/md";
 
 const NavBar = () => {
+  const [nav, setNav] = useState(false)
     const links = [
         {
           id: 1,
@@ -40,8 +42,21 @@ const NavBar = () => {
               </li>
           ))}
         </ul>
+        <div onClick={()=>setNav(!nav)} className='pr-4 cursor-pointer z-50 md:hidden '>
+        {nav ? <MdClose size={35}/>: <CiMenuFries  size={35} />}
+        </div>
         <div>
-        <CiMenuFries  size={35} className='flex md:hidden'/>
+          {nav&& (
+            <ul className='flex flex-col justify-center items-center absolute top-0 
+            left-0 w-full h-screen bg-gradient-to-b from-black to-gray-500'>
+              {links.map(link=>(
+              <li className='px-4 cursor-pointer capitalize py-6 text-4xl' onClick={()=>setNav(!nav)} key={link.id}>
+                  <Link to={link.link} smooth duration={800} >{link.link}</Link>
+                </li>
+                 ))}
+            </ul>
+          )}
+          
         </div>
       </div>
     </div>
