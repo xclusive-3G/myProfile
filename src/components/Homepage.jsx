cc29e4f025161cd7f559bac73ad2ce34182de61c
unknown <akinrataa@gmail.com>
2024-11-17 11:48:31 +0100

    new update on the homepage

--- src/components/Homepage.jsx
+++ src/components/Homepage.jsx
@@ -1,6 +1,7 @@
 import React from 'react'
-import  homeImage  from './Assets/images/displayImage.jpeg'
+import homeImage from './Assets/images/displayImage.jpeg'
 import { FaArrowRight } from "react-icons/fa6";
+import { Link } from "react-scroll"
 
 export const Homepage = () => {
   //const {navBar,NavSetState} = false
@@ -13,14 +14,18 @@ export const Homepage = () => {
           <p className='text-white'>I have 3 years of experience building and designing software. Currently,
             i love to work on web application using technology like React,Tailwind, Node Js, python and Mongo-DB.</p>
           <p>
-            <button className='flex p-2 text-white bg-green-500 my-3 rounded-md font-bold group'>
-              <span className='p-1.5 '>Portfolio</span> 
-              <FaArrowRight className='mt-3 mx-1 group-hover:rotate-90 duration-300'/>
+          <Link to='portfolio' smooth duration={800}>
+            <button  className='flex p-2 text-white bg-green-500 my-3 rounded-md font-bold group'>
+             
+                <span className='p-1.5 '>Portfolio</span>
+                <FaArrowRight className='mt-3 mx-1 group-hover:rotate-90 duration-300' />
+              
             </button>
+            </Link>
           </p>
         </div>
         <div className='order-1 mt-24 md:mt-2 pt-3 md:pt-0 md:order-2'>
-          <img className='mx-auto rounded-2xl w-full h-96 md:w-96 md:ml-10 ' src= {homeImage} alt="HomeImage" srcset="" />
+          <img className='mx-auto rounded-2xl w-full h-96 md:w-96 md:ml-10 ' src={homeImage} alt="HomeImage" srcset="" />
         </div>
       </div>
     </div>
